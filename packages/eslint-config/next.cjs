ef06a975ab42c797abea1d82098e994767eb41e7
Charles-Edouard Fatrane <cfatranedev@gmail.com>
2025-05-03 15:19:32 +0200

    chore: create new base config with cjs and eslint 8 style, add basic config fields to next

--- packages/eslint-config/next.cjs
+++ packages/eslint-config/next.cjs
@@ -1,8 +1,30 @@
+const baseConfig = require("./base.cjs");
+
 /* Custom ESLint configuration for use with Next.js apps. */
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  ...baseConfig,
   root: true,
-  extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier",
+  ],
+  plugins: ["react", "@typescript-eslint", "@next/next"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    serviceworker: true,
+  },
   settings: {
     react: {
       version: "detect",
