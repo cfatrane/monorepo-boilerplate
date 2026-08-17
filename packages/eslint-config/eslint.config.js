f88c47b5e60abbdb083fd7eb54a0b57586eb8930
Charles-Édouard Fatrane <17748314+cfatrane@users.noreply.github.com>
2026-08-17 21:54:14 +0200

    chore(eslint): upgrade workspace to ESLint 10 (#145)

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -15,5 +14,4 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
 ]);
